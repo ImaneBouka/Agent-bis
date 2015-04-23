cb1c1f9618d934667a58402a2f1275cf728252a0
u6028908 <imane.bouka@thomsonreuters.com>
2015-04-23 15:17:19 +0200

    Hostname

--- Http/httpSender.js
+++ Http/httpSender.js
@@ -5,7 +5,7 @@
 var fs = require('fs');
 var request = require('request');
 var options = {
-    url: 'http://localhost:3003/api',
+    url: 'http://127.0.0.1:3003/api',
     method: 'POST',
     json: true,
     body: {titi: 1}

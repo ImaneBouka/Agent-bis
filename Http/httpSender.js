7785f796f4424760eeb7e91eea1ecda49f806bc7
u6028908 <imane.bouka@thomsonreuters.com>
2015-04-23 17:03:21 +0200

    hostname redirects to us2o-ctarbmq-01.amers2.cis.trcloud:5671

--- Http/httpSender.js
+++ Http/httpSender.js
@@ -5,7 +5,7 @@
 var fs = require('fs');
 var request = require('request');
 var options = {
-    url: 'http://127.0.0.1:3003/api',
+    url: 'http://164.57.193.11:5671/api',
     method: 'POST',
     json: true,
     body: {titi: 1}
